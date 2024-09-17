db35be0f1cbb6307c1032597a03035c17da6c3f1
Tarun Jaiswal <tarunjais28@gamil.com>
2024-09-18 02:15:49 +0530

    Optimisation

--- solution/src/bin/application.rs
+++ solution/src/bin/application.rs
@@ -1,13 +1,13 @@
+use colored::*;
 use std::io::{self, prelude::*};
+use std::time::Instant;
 
 use todo_swamp::*;
 
 pub fn main() {
-    // List for todo
-    let mut tl: TodoList = TodoList::new();
-    // List for completed Tasks
-    let mut cl: Vec<TodoItem> = Vec::new();
+    let start_main = Instant::now();
 
+    let start = Instant::now();
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).expect("Failed to read line");
@@ -17,24 +17,51 @@ pub fn main() {
         .parse::<usize>()
         .expect("Error getting number of queries: invalid input!");
 
+    let mut tl: TodoList = TodoList::new(no_of_queries);
+
     // Collect all the inputs first
-    let mut inputs = Vec::new();
+    let mut inputs = Vec::with_capacity(no_of_queries);
     for line in stdin.lock().lines().take(no_of_queries).flatten() {
         inputs.push(line);
     }
+    let duration = start.elapsed();
+    println!(
+        "{}",
+        &format!("Time taken for read: {:?}", duration).yellow()
+    );
 
+    let start = Instant::now();
     // Buffer to store the results
     let mut output_buffer = Vec::new();
 
     // Process all queries in a batch
     for line in inputs {
-        if let Some(result) = runner::run_line(&line, &mut tl, &mut cl) {
+        if let Some(result) = runner::run_line(&line, &mut tl) {
             output_buffer.push(result);
         }
     }
+    let duration = start.elapsed();
+    println!(
+        "{}",
+        &format!("Time taken for process: {:?}", duration).yellow()
+    );
 
+    let start = Instant::now();
     // Output results in one go
     for output in output_buffer {
         println!("{}", output);
     }
+    let duration = start.elapsed();
+    println!(
+        "{}",
+        &format!("Time taken for write: {:?}", duration).yellow()
+    );
+
+    let duration_main = start_main.elapsed();
+    println!(
+        "{}",
+        &format!("Time taken for main: {:?}", duration_main)
+            .green()
+            .bold()
+    );
 }
--- solution/src/runner.rs
+++ solution/src/runner.rs
@@ -1,8 +1,8 @@
 use crate::*;
 
-pub fn run_line(line: &str, tl: &mut TodoList, cl: &mut Vec<TodoItem>) -> Option<String> {
+pub fn run_line(line: &str, tl: &mut TodoList) -> Option<String> {
     if let Ok((_, q)) = parser::query(line) {
-        match run_query(q, tl, cl) {
+        match run_query(q, tl) {
             Ok(r) => Some(format!("{}", r)),
             Err(e) => Some(format!("Error: {}", e)),
         }
@@ -11,23 +11,19 @@ pub fn run_line(line: &str, tl: &mut TodoList, cl: &mut Vec<TodoItem>) -> Option
     }
 }
 
-fn run_query<'a>(
-    q: Query,
-    tl: &'a mut TodoList,
-    cl: &'a mut Vec<TodoItem>,
-) -> Result<QueryResult<'a>, QueryError> {
+fn run_query(q: Query, tl: &mut TodoList) -> Result<QueryResult, QueryError> {
     match q {
         Query::Add(desc, tags) => {
             let item = tl.push(desc, tags);
             Ok(QueryResult::Added(item))
         }
         Query::Done(idx) => {
-            tl.done_with_index(idx, cl);
+            tl.done_with_index(idx);
             Ok(QueryResult::Done)
         }
         Query::Search(params) => {
-            let items = tl.search(params);
-            Ok(QueryResult::Found(items))
+            let indices = tl.search(params);
+            Ok(QueryResult::Found(indices))
         }
     }
 }
